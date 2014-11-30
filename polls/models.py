cadf8df9890f6219d5da9840c9b1273b90b15cd8
crimson16 <tyler.rocca@gmail.com>
2014-11-30 16:54:08 -0500

    here

--- polls/models.py
+++ polls/models.py
@@ -26,7 +26,7 @@ class SenateStats(models.Model):
     state = models.CharField(max_length=200, blank=True, null=True)
     title = models.CharField(max_length=200, blank=True, null=True)
     topic = models.CharField(max_length=200, blank=True, null=True)
-    title = models.CharField(max_length=200, blank=True, null=True)
+    # title = models.CharField(max_length=200, blank=True, null=True)
     url = models.CharField(max_length=200, blank=True, null=True)
     num_cand = models.IntegerField(default=0, blank=True, null=True)
 
@@ -45,6 +45,76 @@ class SenateStats(models.Model):
     ind_val = models.DecimalField(default = 0, decimal_places=3, max_digits= 6, blank=True, null=True)
     ind_lead_conf = models.DecimalField(default = 0, decimal_places=3, max_digits= 6, blank=True, null=True)
     incumbent = models.CharField(default = "", max_length=200, blank=True, null=True)
+
+class PresidentStats(models.Model):
+    election_date = models.CharField(max_length=200, blank=True, null=True)
+    last_updated = models.CharField(max_length=200, blank=True, null=True)
+    poll_count = models.IntegerField(default=0, blank=True, null=True)
+    short_title = models.CharField(max_length=200, blank=True, null=True)
+    slug = models.CharField(max_length=200, blank=True, null=True)
+    state = models.CharField(max_length=200, blank=True, null=True)
+    #Abbreviations
+
+    title = models.CharField(max_length=200, blank=True, null=True)
+    topic = models.CharField(max_length=200, blank=True, null=True)
+    #title = models.CharField(max_length=200, blank=True, null=True)
+    #THIS was done twice in the senate model! Will it cause a problem??
+    url = models.CharField(max_length=200, blank=True, null=True)
+    num_cand = models.IntegerField(default=0, blank=True, null=True)
+    #not in poll data but is it just the number of canidates?
+
+    rep_name_first = models.CharField(max_length=200, blank=True, null=True)
+    rep_name_last = models.CharField(max_length=200, blank=True, null=True)
+    rep_val = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+    rep_lead_conf = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+
+
+    dem_name_first = models.CharField(max_length=200, blank=True, null=True)
+    dem_name_last = models.CharField(max_length=200, blank=True, null=True)
+    dem_val = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+    dem_lead_conf = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+
+    ind_name_first = models.CharField( default = "", max_length=200, blank=True, null=True)
+    ind_name_last = models.CharField(default = "", max_length=200, blank=True, null=True)
+    ind_val = models.DecimalField(default = 0, decimal_places=3, max_digits= 6, blank=True, null=True)
+    ind_lead_conf = models.DecimalField(default = 0, decimal_places=3, max_digits= 6, blank=True, null=True)
+    incumbent = models.CharField(default = "", max_length=200, blank=True, null=True)
+
+class HouseStats(models.Model):
+
+    election_date = models.CharField(max_length=200, blank=True, null=True)
+    last_updated = models.CharField(max_length=200, blank=True, null=True)
+    poll_count = models.IntegerField(default=0, blank=True, null=True)
+    short_title = models.CharField(max_length=200, blank=True, null=True)
+    slug = models.CharField(max_length=200, blank=True, null=True)
+    state = models.CharField(max_length=200, blank=True, null=True)
+    #Abbreviations
+
+    title = models.CharField(max_length=200, blank=True, null=True)
+    topic = models.CharField(max_length=200, blank=True, null=True)
+    #title = models.CharField(max_length=200, blank=True, null=True)
+    #THIS was done twice in the senate model! Will it cause a problem??
+    url = models.CharField(max_length=200, blank=True, null=True)
+    num_cand = models.IntegerField(default=0, blank=True, null=True)
+    #not in poll data but is it just the number of canidates?
+
+    rep_name_first = models.CharField(max_length=200, blank=True, null=True)
+    rep_name_last = models.CharField(max_length=200, blank=True, null=True)
+    rep_val = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+    rep_lead_conf = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+
+
+    dem_name_first = models.CharField(max_length=200, blank=True, null=True)
+    dem_name_last = models.CharField(max_length=200, blank=True, null=True)
+    dem_val = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+    dem_lead_conf = models.DecimalField(decimal_places=3, max_digits= 6, blank=True, null=True)
+
+    ind_name_first = models.CharField( default = "", max_length=200, blank=True, null=True)
+    ind_name_last = models.CharField(default = "", max_length=200, blank=True, null=True)
+    ind_val = models.DecimalField(default = 0, decimal_places=3, max_digits= 6, blank=True, null=True)
+    ind_lead_conf = models.DecimalField(default = 0, decimal_places=3, max_digits= 6, blank=True, null=True)
+    incumbent = models.CharField(default = "", max_length=200, blank=True, null=True)
+    
 
 
 
